abe9da64239fdb1b70669570bc612186f5cca29a
신원식 <action_123@naver.com>
2024-03-15 20:05:24 +0900

    modified:   src/main/java/com/example/askproject/Service/PageService.java

--- src/main/java/com/example/askproject/Service/PageService.java
+++ src/main/java/com/example/askproject/Service/PageService.java
@@ -4,27 +4,18 @@ import java.util.List;
 
 import com.example.askproject.Model.DTO.PageDTO;
 
-import jakarta.servlet.http.HttpServletRequest;
-
 public interface PageService {
-    void insertPage(PageDTO pageDTO);
+    public void insertPage(PageDTO pageDTO);
 
-    void deletePage(String pageId);
+    public void deletePage(String pageId);
 
-    void updatePage(PageDTO pageDTO);
+    public void updatePage(PageDTO pageDTO);
 
-    List<PageDTO> findAllPage();
+    public List<PageDTO> findAllPage();
 
-    PageDTO findByPageId(String pageId);
+    public PageDTO findByPageId(String pageId);
 
-    List<String> findByPageIdContaining(String keyword);
-
-    void increasePageCount(String pageId);
-
-<<<<<<< HEAD
     public List<String> findByPageIdContaining(String keyword);
 
 
-=======
->>>>>>> aa496227f1dd16eb64c4692c600ca5110228cac7
 }
