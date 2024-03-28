347d8803d7b7d46b6e4a4df4754d1f6035f05163
Sophie Kwon <mmloo0525@gmail.com>
2024-03-28 16:01:25 +0900

    modified:   src/main/java/com/example/askproject/Controller/LoginController.java 	new file:   src/main/resources/static/assets/js/admin.js 	new file:   src/main/resources/static/assets/js/auth.js 	new file:   src/main/resources/static/assets/js/etc.js 	deleted:    src/main/resources/static/css/footer.html 	deleted:    src/main/resources/static/css/header.html 	renamed:    src/main/resources/templates/main.html -> src/main/resources/templates/admin/main.html 	renamed:    src/main/resources/templates/mypage.html -> src/main/resources/templates/admin/mypage.html 	renamed:    src/main/resources/templates/pagesetting.html -> src/main/resources/templates/admin/pagesetting.html 	renamed:    src/main/resources/templates/login.html -> src/main/resources/templates/auth/login.html 	renamed:    src/main/resources/templates/regist.html -> src/main/resources/templates/auth/regist.html 	renamed:    src/main/resources/templates/error.html -> src/main/resources/templates/etc/error.html 	renamed:    src/main/resources/templates/index.html -> src/main/resources/templates/etc/index.html 	renamed:    src/main/resources/templates/messageRedirect.html -> src/main/resources/templates/etc/messageRedirect.html 	new file:   src/main/resources/templates/user/main.html 	new file:   src/main/resources/templates/user/mypage.html 	new file:   src/main/resources/templates/user/pagesetting.html

--- src/main/java/com/example/askproject/Controller/LoginController.java
+++ src/main/java/com/example/askproject/Controller/LoginController.java
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
-
     @GetMapping("/login")
-    public String showLoginPage(@RequestParam(value = "error", required = false)String error, @RequestParam(value = "exception", required = false)String exception, Model model) {
+    public String showLoginPage(@RequestParam(value = "error", required = false) String error,
+            @RequestParam(value = "exception", required = false) String exception, Model model) {
         model.addAttribute("error", error);
         model.addAttribute("exception", exception);
         return "login";
     }
-
+9
     @GetMapping("/")
     public String getIndexPage() {
         return "index";
