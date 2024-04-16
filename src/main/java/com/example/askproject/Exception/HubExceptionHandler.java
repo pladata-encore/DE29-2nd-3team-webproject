b31986d61c71b976e186ca32e417ee9c03149f2b
신원식 <action_123@naver.com>
2024-04-16 16:16:56 +0900

    modified:   src/main/java/com/example/askproject/Exception/HubExceptionHandler.java

--- src/main/java/com/example/askproject/Exception/HubExceptionHandler.java
+++ src/main/java/com/example/askproject/Exception/HubExceptionHandler.java
@@ -11,23 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @ControllerAdvice
 public class HubExceptionHandler {
-    @ExceptionHandler(value = UserException.class)
-    public void UserExceptionHandler(UserException e, HttpServletResponse response) throws IOException{
-        ExceptionTypes exceptionTypes = ExceptionTypes.valueOf(e.getClass().getSimpleName());
-        String errorMessage = exceptionTypes.getMsg();
-        log.error("message: " + errorMessage);
-        errorMessage = URLEncoder.encode(errorMessage, "UTF-8");
-        response.sendRedirect("/?error=true&exception="+errorMessage);
-    }
-
-    @ExceptionHandler(value = PageException.class)
-    public void ServiceExceptionHandler(PageException e, HttpServletResponse response) throws IOException{
-        ExceptionTypes exceptionTypes = ExceptionTypes.valueOf(e.getClass().getSimpleName());
-        String errorMessage = exceptionTypes.getMsg();
-        log.error("message: " + errorMessage);
-        errorMessage = URLEncoder.encode(errorMessage, "UTF-8");
-        response.sendRedirect("/?error=true&exception="+errorMessage);
-    }
+    
     @ExceptionHandler(value = Exception.class)
     public void ExceptionHandler(Exception e, HttpServletResponse response) throws IOException{
         ExceptionTypes exceptionTypes = ExceptionTypes.valueOf(e.getClass().getSimpleName());
